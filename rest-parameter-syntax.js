14ddd40e32f40ca071ea0a05421592961ea34a90
CozyPlantlady <laurajohanna.taipale@gmail.com>
2022-08-19 08:18:27 +0000

    Initial commit

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -3,8 +3,28 @@
  * command node rest-parameter-syntax.js in the terminal
  */
 
+
 // Regular function call 
+const sumAll= (a, b, c) => a + b + c;
+let sum = sumAll(1, 2, 3);
+console.log("Sum1:", sum);
+// Sum: 6
+
 
 // Extra arguments are ignored
+let sum2 = sumAll(1, 2, 3, 4, 5, 6);
+console.log("Sum2:", sum2);
+// SUm: 6 (rest is ignored)
+
 
 // Function using ...rest
+const sumRest = (a, b, c, ...rest) => {
+    let sum = a + b + c;
+    for (let i of rest) {
+        sum += i
+    }
+    return sum;
+}
+
+let sum3 = sumRest(1, 2, 3, 4, 5, 6);
+console.log("Sum3;", sum3);
